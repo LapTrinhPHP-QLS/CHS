74cf20dfd2cc0f80a2f210de225cc1287a1d681d
Trần Ngọc Nam <ngocnam071094@gmail.com>
2017-06-08 14:29:19 +0700

    Delete config.php

--- Soure code/modules/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	  $tenmaychu='localhost';
-	  $tentaikhoan='root';
-	  $pass='';
-	  $csdl='qls';
-	  $conn=mysql_connect($tenmaychu,$tentaikhoan,$pass,$csdl) or die('Không kết nối được');
-	  mysql_select_db($csdl,$conn);
-?>
